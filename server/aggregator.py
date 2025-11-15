ee50a3bfdc2e660ee5d2c0b2813959ff0725585f
Kian Haddad <haddad.kianm@gmail.com>
2025-11-15 17:40:20 -0500

    apply weighted average of client deltas to global model and add receive_update debug logging

--- server/aggregator.py
+++ server/aggregator.py
@@ -19,17 +19,23 @@ class Aggregator:
     def aggregate(self):
         if len(self.updates) == 0:
             return
+        # We'll treat incoming 'weights' as updates/deltas (local - global).
+        # Compute weighted average delta and add it to the current global model.
+        global_state = deepcopy(self.global_model.state_dict())
 
-        new_state = deepcopy(self.global_model.state_dict())
-
-        # Weighted averaging
+        # Weighted averaging of deltas
         total_data = sum(self.data_sizes)
 
-        for key in new_state.keys():
-            new_state[key] = sum(
+        avg_delta = {}
+        for key in global_state.keys():
+            avg_delta_tensor = sum(
                 (torch.tensor(update[key]) * (ds / total_data))
                 for update, ds in zip(self.updates, self.data_sizes)
             )
+            avg_delta[key] = avg_delta_tensor
+
+        # Apply averaged delta: global + avg_delta
+        new_state = {k: (global_state[k].float() + avg_delta[k].float()) for k in global_state.keys()}
 
         self.global_model.load_state_dict(new_state)
         self.current_round += 1
